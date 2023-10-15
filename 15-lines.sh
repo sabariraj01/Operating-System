13fc41b1c128b21873fb93f9222c81107b471b9a
S Sabari raj <114046096+sabariraj01@users.noreply.github.com>
2023-10-16 02:43:19 +0530

    Create 15-lines.sh

---
+++ 15-lines.sh
@@ -0,0 +1,28 @@
+# Write a shell script like a more command. It asks the user Name, the Name of the file on
+# command prompt and displays only the 15 lines of the file at a time on the screen. Further, next
+# 15 lines will be displayed only when the user presses the enter key / any other key.
+
+counter=1
+echo -n "Enter a file name : "
+read file
+
+if  [ ! -f $file ]
+then
+    echo "$file not a file!"
+    exit 1
+fi
+
+# read file line by line
+exec 3<&0
+while read line
+do
+       # pause at line no. 15
+    if [ $counter -eq 15 ]
+    then
+        counter=0 # reset counter
+        echo "Press Enter key to continue ..."
+        read -u 3 enterKey
+    fi
+    echo $line
+    (( counter++ ))
+done < $file
