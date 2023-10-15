7d6579003a29ef688b0a872341e5f11860f9b86a
S Sabari raj <114046096+sabariraj01@users.noreply.github.com>
2023-10-16 02:53:16 +0530

    Create sum-of-digits.sh

---
+++ sum-of-digits.sh
@@ -0,0 +1,15 @@
+# Write a script to calculate the sum of digits of the given number.
+
+echo "Enter a number"
+read num
+
+sum=0
+
+while [ $num -gt 0 ]
+do
+    mod=$((num % 10))    #It will split each digits
+    sum=$((sum + mod))   #Add each digit to sum
+    num=$((num / 10))    #divide num by 10.
+done
+
+echo $sum
