280743bcea7e6a5700831d5ecff8a5935c960bd6
S Sabari raj <114046096+sabariraj01@users.noreply.github.com>
2023-10-16 02:56:06 +0530

    Create prime-or-not.sh

---
+++ prime-or-not.sh
@@ -0,0 +1,13 @@
+# Write a script to check whether the given number is prime or not
+
+echo enter n
+read n
+num=0
+while [ $n -gt 0 ]
+do
+num=$(expr $num \* 10)
+k=$(expr $n % 10)
+num=$(expr $num + $k)
+n=$(expr $n / 10)
+done
+echo reverse of number is $num
